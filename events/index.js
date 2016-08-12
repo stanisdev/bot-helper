ce82a5cf75bd70b82d64a156400554033506267d
Stanislav Zavalishin <stan-coder@gmail.com>
2016-08-12 14:10:52 +0600

    fix

--- events/index.js
+++ events/index.js
@@ -34,7 +34,25 @@ module.exports = function(config) {
           json: true
         };
         request.post(chatData, function() {
-          console.log('****DONE*****');
+
+          var sqlite3 = require('sqlite3').verbose();
+          var db = new sqlite3.Database('users');
+
+          db.serialize(function() {
+            db.run("CREATE TABLE lorem (info TEXT)");
+
+            var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+            for (var i = 0; i < 10; i++) {
+                stmt.run("Ipsum " + i);
+            }
+            stmt.finalize();
+
+            db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+                console.log(row.id + ": " + row.info);
+            });
+            db.close();
+          });
+
           cb({ success: true });
         });
       } else {
