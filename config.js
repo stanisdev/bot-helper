4012c3ac4fbcb39fb26819274c87b9b9009fa1e1
Stanislav Zavalishin <stan-coder@gmail.com>
2016-08-15 15:25:15 +0600

    Fix

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = function() {
     port: process.env.PORT || 3000,
     token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTIsInBob25lIjoiOTk2NTUyNzg0MDg2IiwicGFzc3dvcmQiOiI3YTgzY2NiZjNiZmFlOGZhOTQyNWZkYWQ0OWNmNjFiMiIsImlhdCI6MTQ3MDk3NTY3N30.6qY9KlL_kgYnx-1hfdZDbgq4RBRVA6RDY38LQGqUzCw',
     api_url: 'http://api.kamp.kg', // 'http://localhost:3000'
-    db_name: 'users',
+    db_name: 'users2',
     bot_name: 'Ваш персональный помощник'
   };
 };
